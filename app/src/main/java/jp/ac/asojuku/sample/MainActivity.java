0cc1ed1f5a1026abfcec6d14b99ef6f5f23378cc
OnikiHub <oniki3030@gmail.com>
2018-10-11 14:32:03 +0900

    hh

--- app/src/main/java/jp/ac/asojuku/sample/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/sample/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+<<<<<<< HEAD
     System.out.pritnln(lolololololololololololol);
+=======
+    System.out.pritnln();
+>>>>>>> a
 }
