2c48b56a3c8f0c722d160b3742e1a71c791b0fa5
God-Matsuo <matsuohibiki1601142@gmail.com>
2018-10-12 14:26:30 +0900

    しまじろう

--- app/src/main/java/jp/ac/asojuku/sample/MainActivity.java
+++ app/src/main/java/jp/ac/asojuku/sample/MainActivity.java
@@ -16,4 +16,6 @@ public class MainActivity extends AppCompatActivity {
     System.out.pritnln();
 >>>>>>> a
      System.out.pritnln(kikikjnnjninoininoinoinoinoinoinionoinononoininoi);
+=======
+     System.out.println(pokekayamero);
 }
